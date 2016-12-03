1d4d555463e1606d31ba31f09bfa88dda63e036d
Alok Singh <alok.s09@gmail.com>
2016-12-03 18:49:16 -0500

    multiple stream test

--- stream-server.js
+++ stream-server.js
@@ -16,8 +16,9 @@ var width = 320,
 	height = 240;
 
 // Websocket Server
-var socketServer = new (require('ws').Server)({port: WEBSOCKET_PORT});
-socketServer.on('connection', function(socket) {
+var socketServerOne = new (require('ws').Server)({port: WEBSOCKET_PORT});
+var socketServerTwo = new (require('ws').Server)({port: WEBSOCKET_PORT+1});
+socketServerOne.on('connection', function(socket) {
 	// Send magic bytes and video size to the newly connected socket
 	// struct { char magic[4]; unsigned short width, height;}
 	var streamHeader = new Buffer(8);
@@ -26,14 +27,14 @@ socketServer.on('connection', function(socket) {
 	streamHeader.writeUInt16BE(height, 6);
 	socket.send(streamHeader, {binary:true});
 
-	console.log( 'New WebSocket Connection ('+socketServer.clients.length+' total)' );
+	console.log( 'New WebSocket Connection ('+socketServerOne.clients.length+' total)' );
 	
 	socket.on('close', function(code, message){
-		console.log( 'Disconnected WebSocket ('+socketServer.clients.length+' total)' );
+		console.log( 'Disconnected WebSocket ('+socketServerOne.clients.length+' total)' );
 	});
 });
 
-socketServer.broadcast = function(data, opts) {
+socketServerOne.broadcast = function(data, opts) {
 	for( var i in this.clients ) {
 		if (this.clients[i].readyState == 1) {
 			this.clients[i].send(data, opts);
@@ -43,6 +44,34 @@ socketServer.broadcast = function(data, opts) {
 		}
 	}
 };
+
+socketServerTwo.on('connection', function(socket) {
+	// Send magic bytes and video size to the newly connected socket
+	// struct { char magic[4]; unsigned short width, height;}
+	var streamHeader = new Buffer(8);
+	streamHeader.write(STREAM_MAGIC_BYTES);
+	streamHeader.writeUInt16BE(width, 4);
+	streamHeader.writeUInt16BE(height, 6);
+	socket.send(streamHeader, {binary:true});
+
+	console.log( 'New WebSocket Connection ('+socketServerTwo.clients.length+' total)' );
+	
+	socket.on('close', function(code, message){
+		console.log( 'Disconnected WebSocket ('+socketServerTwo.clients.length+' total)' );
+	});
+});
+
+socketServerTwo.broadcast = function(data, opts) {
+	for( var i in this.clients ) {
+		if (this.clients[i].readyState == 1) {
+			this.clients[i].send(data, opts);
+		}
+		else {
+			console.log( 'Error: Client ('+i+') not connected.' );
+		}
+	}
+};
+
 
 
 // HTTP Server to accept incomming MPEG Stream
@@ -59,9 +88,23 @@ var streamServer = require('http').createServer( function(request, response) {
 			'Stream Connected: ' + request.socket.remoteAddress + 
 			':' + request.socket.remotePort + ' size: ' + width + 'x' + height
 		);
-		request.on('data', function(data){
-			socketServer.broadcast(data, {binary:true});
-		});
+		if (params[3] == "webcam") {
+			console.log("capture Webcam");
+			request.on('data', function(data){
+				socketServerOne.broadcast(data, {binary:true});
+			});
+		}
+		else if (params[3] == "desktop") {
+			console.log("capture desktop");
+			request.on('data', function(data){
+				socketServerTwo.broadcast(data, {binary:true});
+			});
+		} else {
+			console.log("no capture specific");
+			request.on('data', function(data){
+				socketServerOne.broadcast(data, {binary:true});
+			});
+		}
 	}
 	else {
 		console.log(
